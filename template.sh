67d6ffd5cbf8ac4badf2b1dc5c76b8599ba8a393
zaidisi786 <syed.ijlal.zaidi@gmail.com>
2026-02-04 23:05:19 -0600

    directory and files added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Ctreate project directories
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory structure and files created successfully..."
+
