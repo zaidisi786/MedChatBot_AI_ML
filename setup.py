013bf98a2f84d345d416fca3b84b55955376257b
zaidisi786 <syed.ijlal.zaidi@gmail.com>
2026-02-05 12:57:00 -0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Syed Zaidi",
+    author_email="syed.ijlal.zaidi@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
